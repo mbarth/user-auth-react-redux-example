37de184dbc67755b1f40737db8c1cbf6f3d4aea5
Marcelo Barth <marcelo.barth@gmail.com>
2016-12-11 21:54:46 -0800

    Reformat code

--- build/webpack/production.js
+++ build/webpack/production.js
@@ -3,24 +3,24 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import webpackConfig     from './_base';
 
 webpackConfig.module.loaders = webpackConfig.module.loaders.map(loader => {
-  if (/css/.test(loader.test)) {
-    const [first, ...rest] = loader.loaders;
+    if (/css/.test(loader.test)) {
+        const [first, ...rest] = loader.loaders;
 
-    loader.loader = ExtractTextPlugin.extract(first, rest.join('!'));
-    delete loader.loaders;
-  }
+        loader.loader = ExtractTextPlugin.extract(first, rest.join('!'));
+        delete loader.loaders;
+    }
 
-  return loader;
+    return loader;
 });
 
 webpackConfig.plugins.push(
-  new ExtractTextPlugin('[name].[contenthash].css'),
-  new webpack.optimize.UglifyJsPlugin({
-    compress : {
-      'unused'    : true,
-      'dead_code' : true
-    }
-  })
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            'unused': true,
+            'dead_code': true
+        }
+    })
 );
 
 export default webpackConfig;
